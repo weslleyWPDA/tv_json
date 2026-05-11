0dbd4d6804e2b40c40c3d65667e799026be8442a
Weslley P Anjos <61725444+weslleyWPDA@users.noreply.github.com>
2026-05-11 14:50:06 -0400

    Update dns.php

--- dns.php
+++ dns.php
@@ -1,25 +1,10 @@
-<?php
-error_reporting(0);
-
-include 'links.php';
-
-$json_response = array();
-
-foreach ($links as $link) {
-    $row_array = array(
-        'DNSName' => $link['title'],
-        'DNSUrl'  => $link['url']
-    );
-
-    array_push($json_response, $row_array);
-}
-
-header('Content-type: application/json; charset=UTF-8');
-
-$final = json_encode(
-    $json_response,
-    JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
-);
-
-echo base64_encode($final);
-?>
+[
+    {
+        "DNSName": "multi",
+        "DNSUrl": "http://ccpd.pro:80"
+    },
+    {
+        "DNSName": "SEM BASE",
+        "DNSUrl": "http://ph1.fun"
+    }
+]
