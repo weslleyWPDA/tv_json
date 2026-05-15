a5e1998ba340349fd5af04c0c28bc099e2e25c71
Weslley P Anjos <61725444+weslleyWPDA@users.noreply.github.com>
2026-05-15 09:41:36 -0400

    Update dns.php

--- smart/api/dns.php
+++ smart/api/dns.php
@@ -1,1 +1,1 @@
-{"ftg":true,"status":true,"su":"http://ccpd.pro,http://redurl.awpsi.com,http://ph1.fun:80","sc":"","ndd":""}
+<html><head></head><body>{"ftg":true,"status":true,"su":"http://ccpd.pro,http://redurl.awpsi.com,http://ph1.fun:80","sc":"","ndd":""}</body></html>
